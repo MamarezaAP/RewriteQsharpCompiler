a25036749f340df3491a906d78b5e2d82ea21d9c
MamarezaAP <mamarezaart@gmail.com>
2021-09-28 13:04:37 +0330

    Add project files.

---
+++ RewriteQsharpCompiler/Case.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.Quantum.QsCompiler;
+using Microsoft.Quantum.QsCompiler.SyntaxTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RewriteQsharpCompiler
+{
+    public class Case : IRewriteStep
+    {
+        private readonly List<IRewriteStep.Diagnostic> diagnostics = new List<IRewriteStep.Diagnostic>();
+        public string Name => "Case-based Access Modifiers";
+        public int Priority => 0;
+        public IDictionary<string, string> AssemblyConstants { get; } = new Dictionary<string, string>();
+        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics => diagnostics;
+        public bool ImplementsPreconditionVerification => true;
+        public bool ImplementsTransformation => true;
+        public bool ImplementsPostconditionVerification => false;
+
+        public bool PreconditionVerification(QsCompilation compilation)
+        {
+            var invalidAccessibility = compilation.Namespaces.Callables()
+                .Where(x => x.SourceFile.EndsWith(".qs"))
+                .Where(x => x.Modifiers.Access.IsDefaultAccess && !char.IsUpper(x.FullName.Name[0]) ||
+                            x.Access.IsInternal && char.IsUpper(x.FullName.Name[0]));
+
+            foreach (var callable in invalidAccessibility)
+            {
+                diagnostics.Add(new IRewriteStep.Diagnostic
+                {
+                    Severity = DiagnosticSeverity.Warning,
+                    Message = $@"Callable '{callable.FullName} should be {(callable.Access.IsPublic ? "internal" : "public")}. This will be auto-corrected.'",
+                    Stage = IRewriteStep.Stage.PreconditionVerification,
+                    Source = callable.SourceFile,
+                    Range = callable.Location.IsValue ? callable.Location.Item.Range : null
+
+                });
+            }
+
+            return true;
+        }
+
+        public bool Transformation(QsCompilation compilation, out QsCompilation transformed)
+        {
+            var rewriter = new RewriteAccessModifiers();
+            transformed = rewriter.OnCompilation(compilation);
+            return true;
+        }
+
+        public bool PostconditionVerification(QsCompilation compilation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RewriteQsharpCompiler/RewriteAccessModifiers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Quantum.QsCompiler.SyntaxTree;
+using Microsoft.Quantum.QsCompiler.Transformations.Core;
+using System;
+
+namespace RewriteQsharpCompiler
+{
+    public class RewriteAccessModifiers : SyntaxTreeTransformation<EmptyState>
+    {
+        public RewriteAccessModifiers() : base(new EmptyState())
+        {
+            Namespaces = new NamespaceTransformation(this);
+        }
+
+        private class NamespaceTransformation : NamespaceTransformation<EmptyState>
+        {
+            public NamespaceTransformation(SyntaxTreeTransformation<EmptyState> parent) : base(parent, TransformationOptions.Default)
+            {}
+
+            public override QsCallable OnCallableDeclaration(QsCallable c)
+            {
+                if (!c.SourceFile.EndsWith(".qs", StringComparison.OrdinalIgnoreCase))
+                    return c;
+                Console.WriteLine($"Callable: {c.FullName} in {c.SourceFile}");
+                var transformed = c.ToCaseDriveAccessModifier();
+                Console.WriteLine($" modifier: {c.Modifiers.Access} -> modifier: {transformed.Modifiers.Access}");
+                return transformed;
+            }
+        }
+    }
+
+    public class EmptyState
+    { }
+}
